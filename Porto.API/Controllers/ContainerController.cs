adf25820553adc07a90cfe0842a3e09fbffedfe0
Jefferson Pita <jefferson.pita@modalgr.com.br>
2020-04-13 14:42:59 -0300

    backend

---
+++ Porto.API/Controllers/ContainerController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Porto.Business.Interfaces;
+using Porto.Business.Model;
+using Porto.Data.DataContext;
+using Porto.Data.Repository;
+
+namespace Porto.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContainerController : Controller
+    {
+        public readonly IContainerRepository _container;
+        public ContainerController(IContainerRepository container)
+        {
+            _container = container;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllContaineres()
+        {
+            try
+            {
+                var resulta = await _container.GetAll();
+                return Ok(resulta);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de dados Falhou");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Container container)
+        {
+            try
+            {
+                _container.Add(container);
+
+                if(await _container.SaveChangesAsync())
+                {
+                    return Created($"/api/container/{container.Id}", container);
+                }
+
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de dados Falhou");
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPut("edit/{id}")]
+        public async Task<IActionResult> Put(int id, Container container)
+        {
+             try
+            {
+                _container.Update(container);
+
+                if(await _container.SaveChangesAsync())
+                {
+                    return Created($"/api/container/{container.Id}", container);
+                }
+
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de dados Falhou");
+            }
+
+            return BadRequest();
+        }
+
+        [HttpDelete("del/{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+              try
+            {
+                var cont = await _container.GetById(id);
+                if(cont == null) return NotFound();
+
+                _container.Delete(cont);
+
+                if(await _container.SaveChangesAsync())
+                {
+                    return Ok();
+                }
+
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de dados Falhou");
+            }
+
+            return BadRequest();
+        }
+
+    }
+}
---
+++ Porto.API/Controllers/MovimentoController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Porto.Business.Interfaces;
+using Porto.Business.Model;
+using Porto.Data.DataContext;
+using Porto.Data.Repository;
+
+namespace Porto.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovimentoController : Controller
+    {
+        private readonly IMovimentoRepository _movimento;
+        public MovimentoController(IMovimentoRepository movimento)
+        {
+            _movimento = movimento;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMovimento()
+        {
+            try
+            {
+                var resulta = await _movimento.GetAll();
+                return Ok(resulta);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de dados Falhou");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Movimento movimento)
+        {
+             try
+            {
+                _movimento.Add(movimento);
+
+                if(await _movimento.SaveChangesAsync())
+                {
+                    return Created($"/api/movimento/{movimento.Id}", movimento);
+                }
+
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de dados Falhou");
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPut("edit/{id}")]
+        public async Task<IActionResult> Put(int id, Movimento movimento)
+        {
+             try
+            {
+                _movimento.Update(movimento);
+
+                if(await _movimento.SaveChangesAsync())
+                {
+                    return Created($"/api/movimento/{movimento.Id}", movimento);
+                }
+
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de dados Falhou");
+            }
+
+            return BadRequest();
+        }
+
+        [HttpDelete("del/{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+              try
+            {
+                var cont = await _movimento.GetById(id);
+                if(cont == null) return NotFound();
+
+                _movimento.Delete(cont);
+
+                if(await _movimento.SaveChangesAsync())
+                {
+                    return Ok();
+                }
+
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de dados Falhou");
+            }
+
+            return BadRequest();
+        }
+    }
+}
